e6c48721ceee0e1d153faa8bbb0e8e33ad9022a7
Basel Sayeh <basel.sayeh@hotmail.com>
2024-04-05 22:20:57 +0300

    simplify the build script

---
+++ clone.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git clone https://github.com/milkv-duo/host-tools                 -b master --single-branch
+git clone https://github.com/milkv-duo/build                                --single-branch
+git clone https://github.com/milkv-duo/freertos freertos                    --single-branch
+git clone https://github.com/milkv-duo/FreeRTOS-Kernel                      --single-branch "freertos/Source"
+git clone https://github.com/milkv-duo/Lab-Project-FreeRTOS-POSIX           --single-branch "freertos/Source/FreeRTOS-Plus-POSIX"
+git clone https://github.com/milkv-duo/fsbl                                 --single-branch
+git clone https://github.com/milkv-duo/opensbi                              --single-branch
+git clone https://github.com/milkv-duo/u-boot-2021.10                       --single-branch
+git clone https://github.com/milkv-duo/linux_5.10                           --single-branch
+git clone https://github.com/milkv-duo/ramdisk                              --single-branch
+git clone https://github.com/milkv-duo/osdrv                                --single-branch
+git clone https://github.com/milkv-duo/middleware                           --single-branch
+git clone https://github.com/milkv-duo/isp_tuning                           --single-branch
+git clone https://github.com/milkv-duo/oss                        -b master --single-branch
+git clone https://github.com/milkv-duo/SensorSupportList          -b v4.1.0 --single-branch "middleware/v2/component/isp"
+git clone https://github.com/milkv-duo/buildroot-2021.05          -b master
